54a687acf7fa7d46fc695772f5a7b1c7578ba045
Ronnie95 <t_daron@yahoo.com>
2025-11-10 19:49:57 -0500

    model creation

--- emailProject/main_app/models.py
+++ emailProject/main_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+
+class EmailInfo(models.model):
+    firstName = models.CharField(max_length=100)
+    lastName = models.CharField(max_length=100)
+    emailAdress = models.EmailField(unique=True)
